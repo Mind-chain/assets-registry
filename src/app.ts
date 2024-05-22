6fdfd9fc63303105da1596b89c1e873692c7370a
Md Asaduzaman <asadasad9282@gmail.com>
2024-05-23 00:32:03 +0600

    changes url on json data

--- src/app.ts
+++ src/app.ts
@@ -1,24 +1,26 @@
-import express, { Request, Response } from 'express';
+import express, { Request, Response } from "express";
 import { readData } from "./fs";
-import cors from 'cors';
+import cors from "cors";
 
 const app = express();
 const PORT = 3000;
 app.use(cors());
 
-readData().then((jsonData: any) => {
+readData()
+  .then((jsonData: any) => {
     const responseData = {
-        status: 1,
-        data: jsonData
+      status: 1,
+      data: jsonData,
     };
 
-    app.get('/', (req: Request, res: Response) => {
-        res.json(responseData);
+    app.get("/", (req: Request, res: Response) => {
+      res.json(responseData);
     });
 
     app.listen(PORT, () => {
-        console.log(`Server is running on http://localhost:${PORT}`);
+      console.log(`Server is running on http://localhost:${PORT}`);
     });
-}).catch((err: any) => {
-    console.error('Error reading data:', err);
-});
+  })
+  .catch((err: any) => {
+    console.error("Error reading data:", err);
+  });
